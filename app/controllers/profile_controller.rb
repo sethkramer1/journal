bc0f441ec5f03d08897c857bccf9bf2d37f9206e
Seth Kramer <sethaepiuva@gmail.com>
2016-11-26 23:03:14 -0500

    trying form collection on pg

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,8 @@
 class ProfileController < ApplicationController
   def index
     @links = Link.where(user_id: current_user).order("created_at DESC").paginate(:page => params[:page], per_page: 7)
+    @counting = Link.where(user_id: current_user).count(:conditions => "title == 😊")
+
   end
+
 end
