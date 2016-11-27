1b9ea6ffd22ee29a9b1c36a2962ce7917854c777
Seth Kramer <sethaepiuva@gmail.com>
2016-11-26 21:23:39 -0500

    added pagination to profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
 class ProfileController < ApplicationController
   def index
-    @links = Link.where(user_id: current_user)
+    @links = Link.where(user_id: current_user).order("created_at DESC").paginate(:page => params[:page], per_page: 7)
   end
 end
